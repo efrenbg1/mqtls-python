2a550aae1e984d76def676f03ffb6181c1f85b1c
Efrén Boyarizo <efren@boyarizo.es>
2020-08-30 18:06:37 +0200

    Added long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='mqtls',
     packages=['mqtls'],
-    version='0.2',
+    version='0.3',
     license='wtfpl',
     description='MqTLS client for python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Efrén Boyarizo',
     author_email='efren@boyarizo.es',
     url='https://github.com/efrenbg1/mqtls-python',
-    download_url='https://github.com/efrenbg1/mqtls-python/archive/0.2.tar.gz',
+    download_url='https://github.com/efrenbg1/mqtls-python/archive/0.3.tar.gz',
     keywords=['MqTLS', 'gobroker', 'client'],
     classifiers=[
         'Development Status :: 3 - Alpha',
