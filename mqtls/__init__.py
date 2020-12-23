4bf1deffcbd24f9295f38ea985fa8e47288f3c98
Efrén Boyarizo <efren@boyarizo.es>
2020-12-23 16:47:04 +0100

    Removed async (useless). Fixed error handling

--- mqtls/__init__.py
+++ mqtls/__init__.py
@@ -2,198 +2,144 @@ import socket
 import ssl
 import threading
 import select
-import queue
 
 
 class mqtls:
-    def __init__(self, host="127.0.0.1", port=2443, user=None, pw=None):
+    def __init__(self, host="127.0.0.1", port=2443, user=None, pw=None, timeout=1):
         self._host = host
         self._port = port
         self._user = user
         self._pw = pw
+        self._timeout = timeout
         self._socket = None
+        self._context = ssl._create_unverified_context()
         self._broker = None
-        self._queue = queue.Queue()
         self._lock = threading.Lock()
+        self._exception = ""
         with self._lock:
             self.__connect()
 
-    def __len2(self, string):
-        return str(len(string)).zfill(2)
+    def __enc(self, string):
+        return str(len(string)).zfill(2) + string
 
     def __connect(self):
-        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._socket.settimeout(10)
-        self._broker = ssl.wrap_socket(self._socket)
-        self._broker.connect((self._host, self._port))
+        self._socket = socket.create_connection((self._host, self._port))
+        # socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self._socket.settimeout(10)
+        self._broker = self._context.wrap_socket(
+            self._socket, server_hostname=self._host)
+        #self._broker.connect((self._host, self._port))
         if not self._broker:
             raise Exception("Could not connect to broker!")
         if self._user and self._pw:
-            rx = None
-            try:
-                self.__send("MQS0" + self.__len2(self._user) +
-                            self._user + self.__len2(self._pw) + self._pw + "1")
-                rx = self.__receive()
-            except Exception:
-                raise Exception("MqTLS: error in auth")
-            if not rx:
-                raise Exception("MqTLS: error in auth")
-            rx = rx.decode("utf-8")
-            if len(rx) < 4:
-                raise Exception("MqTLS: error in auth")
-            if rx[:4] != "MQS0":
-                raise Exception("MqTLS: error in auth")
+            self.__login()
+
+    def __login(self):
+        msg = "MQS0" + self.__enc(self._user) + self.__enc(self._pw) + "1"
+        self.__send(msg)
+        rx = self.__receive()
+        if rx is None:
+            raise Exception(
+                "MqTLS: error in auth ({})".format(self._exception))
+        if len(rx) < 4:
+            raise Exception(
+                "MqTLS: error in auth (invalid response from server)")
+        if rx[:4] != "MQS0":
+            raise Exception("MqTLS: error in auth (invalid credentials)")
 
     def __send(self, data):
         self._broker.send(str.encode(data + '\n'))
 
     def __receive(self):
         buff = b''
-        ready = select.select([self._broker], [], [], 1)
-        if ready[0]:
+        ready = select.select([self._broker], [], [], self._timeout)
+        if not ready[0]:
+            self._exception = "timed out while waiting for response! Is broker up?"
+            return None
+        try:
             rx = self._broker.recv(1)
-        else:
-            raise Exception(
-                "Timed out while waiting for response! Is broker up?")
-        while rx != b'\n':
-            if rx == b'':
-                raise Exception("Read invalid character!")
-            buff += rx
-            if len(buff) > 210:
-                break
-            rx = self._broker.recv(1)
-        return buff
+            while rx != b'\n':
+                if rx == b'':
+                    self._exception = "connection closed by broker"
+                    return None
+                buff += rx
+                if len(buff) > 210:
+                    break
+                rx = self._broker.recv(1)
+            return buff.decode('utf-8')
+        except Exception as e:
+            self._exception = str(e)
+            return None
+
+    def __communicate(self, data):
+        with self._lock:
+            # Clean data before communicating
+            while select.select([self._broker], [], [], 0)[0]:
+                self.__receive()
+
+            # Send data and read response
+            self.__send(data)
+            rx = self.__receive()
+
+            # If no data received, retry
+            if rx is None:
+                self.__connect()
+                self.__send(data)
+                rx = self.__receive()
+
+            return rx
 
     def publish(self, topic, slot, message):
         if self._user is None:
+            msg = "MQS6"
+        else:
+            msg = "MQS1"
+        msg += self.__enc(topic) + str(slot) + self.__enc(message)
+        rx = self.__communicate(msg)
+
+        if rx is None:
             raise Exception(
-                "MqTLS: error in publish, use mpublish in master mode")
-        msg = "MQS1" + self.__len2(topic) + topic + \
-            str(slot) + self.__len2(message) + message
-        rx = None
-        with self._lock:
-            try:
-                self.__send(msg)
-                rx = self.__receive()
-            except Exception:
-                self.__connect()
-                self.__send(msg)
-                rx = self.__receive()
-            finally:
-                if rx is None:
-                    raise Exception("MqTLS: error in publish")
-                rx = rx.decode("utf-8")
-                if len(rx) < 4:
-                    return False
-                if rx[:4] == "MQS1":
-                    return True
+                "MqTLS: error in publish ({})".format(self._exception))
+        if len(rx) < 4:
+            return False
+        if rx[:4] == "MQS1":
+            return True
 
     def retrieve(self, topic, slot):
         if self._user is None:
+            msg = "MQS7"
+        else:
+            msg = "MQS2"
+        msg += self.__enc(topic) + str(slot)
+        rx = self.__communicate(msg)
+        if rx is None:
             raise Exception(
-                "MqTLS: error in retrieve, use mretrieve in master mode")
-        msg = "MQS7" + self.__len2(topic) + topic + str(slot)
-        rx = None
-        with self._lock:
-            try:
-                self.__send(msg)
-                rx = self.__receive()
-            except Exception:
-                self.__connect()
-                self.__send(msg)
-                rx = self.__receive()
-            finally:
-                if rx is None:
-                    raise Exception("MqTLS: error in retrieve")
-                rx = rx.decode("utf-8")
-                if len(rx) < 4:
-                    return None
-                if rx[:4] == "MQS7":
-                    return None
-                if rx[:4] == "MQS2":
-                    return rx[6:6+int(rx[4:6])]
-
-    def mpublish(self, topic, slot, message):
-        msg = "MQS6" + self.__len2(topic) + topic + \
-            str(slot) + self.__len2(message) + message
-        rx = None
-        with self._lock:
-            try:
-                self.__send(msg)
-                rx = self.__receive()
-            except Exception:
-                self.__connect()
-                self.__send(msg)
-                rx = self.__receive()
-            finally:
-                if rx is None:
-                    raise Exception("MqTLS: error in mpublish")
-                rx = rx.decode("utf-8")
-                if len(rx) < 4:
-                    return False
-                if rx[:4] == "MQS6":
-                    return True
-
-    def mretrieve(self, topic, slot):
-        msg = "MQS7" + self.__len2(topic) + topic + str(slot)
-        rx = None
-        with self._lock:
-            try:
-                self.__send(msg)
-                rx = self.__receive()
-            except Exception:
-                self.__connect()
-                self.__send(msg)
-                rx = self.__receive()
-            finally:
-                if rx is None:
-                    raise Exception("MqTLS: error in mretrieve")
-                rx = rx.decode("utf-8")
-                if len(rx) < 4:
-                    return None
-                if rx[:4] == "MQS7":
-                    return None
-                if rx[:4] == "MQS2":
-                    return rx[6:6+int(rx[4:6])]
+                "MqTLS: error in retrieve ({})".format(self._exception))
+        if len(rx) < 4:
+            return None
+        if rx[:4] == "MQS7":
+            return None
+        if rx[:4] == "MQS2":
+            return rx[6:6+int(rx[4:6])]
 
     def muser(self, user):
-        msg = "MQS8" + self.__len2(user) + user
-        rx = None
-        with self._lock:
-            try:
-                self.__send(msg)
-                rx = self.__receive()
-            except Exception:
-                self.__connect()
-                self.__send(msg)
-                rx = self.__receive()
-            finally:
-                if rx is None:
-                    raise Exception("MqTLS: error in user update")
-                rx = rx.decode("utf-8")
-                if len(rx) < 4:
-                    return False
-                if rx[:4] == "MQS8":
-                    return True
-                return False
+        msg = "MQS8" + self.__enc(user)
+        rx = self.__communicate(msg)
+        if rx is None:
+            raise Exception("MqTLS: error in user update")
+        if len(rx) < 4:
+            return False
+        if rx[:4] == "MQS8":
+            return True
+        return False
 
     def macls(self, user):
-        msg = "MQS9" + self.__len2(user) + user
-        rx = None
-        with self._lock:
-            try:
-                self.__send(msg)
-                rx = self.__receive()
-            except Exception:
-                self.__connect()
-                self.__send(msg)
-                rx = self.__receive()
-            finally:
-                if rx is None:
-                    raise Exception("MqTLS: error in acls update")
-                rx = rx.decode("utf-8")
-                if len(rx) < 4:
-                    return False
-                if rx[:4] == "MQS9":
-                    return True
-                return False
+        msg = "MQS9" + self.__enc(user)
+        rx = self.__communicate(msg)
+        if rx is None:
+            raise Exception("MqTLS: error in acls update")
+        if len(rx) < 4:
+            return False
+        if rx[:4] == "MQS9":
+            return True
+        return False
