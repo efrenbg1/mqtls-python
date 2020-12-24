b9b814124e8f02a782297c9aaaa5dcb4b4b537d8
efrenbg1 <efren@boyarizo.es>
2020-12-24 12:38:08 +0100

    Updated

--- test.py
+++ test.py
@@ -4,6 +4,4 @@ import time
 broker = mqtls(host="rmote.app", port=2443,
                user="efren@boyarizo.es", pw="1Q2w3e4r")
 
-while True:
-    print(broker.publish("A4:CF:12:F4:DE:E0", 1, "0"))
-    time.sleep(10)
+print(broker.publish("topic", 3, "6"))
